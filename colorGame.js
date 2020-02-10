1d7371f09dec2f88293f48dac55fd9b45aff84a8
Flare200 <firebird567@cox.net>
2020-02-10 14:05:51 -0800

    Added the functionality for random colors to be generated instead of the hard coded valuees

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
-var colors=[
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors=generateRandomColors(6)
 
 var squares=document.querySelectorAll(".square")
 var pickedColor=pickColor()
 var colorDisplay=document.querySelector("#colorDisplay")
 var msgDisplay=document.querySelector("#msg")
+var h1=document.querySelector("h1")
 
 colorDisplay.textContent=pickedColor
 
@@ -29,6 +23,7 @@ for(var i=0; i<squares.length; i++)
         {
             msgDisplay.textContent="Correct!"
             changeColors(pickedColor)
+            h1.style.backgroundColor=pickedColor
         }
         else
         {
@@ -52,4 +47,30 @@ function pickColor()
 {
     var random=Math.floor(Math.random()*colors.length)
     return colors[random]
+}
+
+function generateRandomColors(num)
+{
+    //Make array
+    var arr=[]
+    //Add num random colors to array
+    for(var i=0; i<num; i++)
+    {
+        //Get random color and push into array
+        arr.push(randomColor())
+    }
+    //Return array
+    return arr
+}
+
+function randomColor()
+{
+    //Pick a "red" from 0-255
+    var r=Math.floor(Math.random()*256)
+    //Pick a "green" from 0-255
+    var g=Math.floor(Math.random()*256)
+    //Pick a "blue" from 0-255
+    var b=Math.floor(Math.random()*256)
+
+    return "rgb("+r+", "+g+", "+b+")"
 }
