acc49d8e9f1d02a6313b7ebad528b89de6eef6d4
Flare200 <firebird567@cox.net>
2020-02-10 12:44:45 -0800

    Added and setup HTML, CSS, and JS files for initial structure and logic
    HTML file is given a title and some divs for the basic structure.
    
    CSS file changes the bg color and sets up the sizes, padding, and margins of the squares
    
    JS file hard codes the squares and the sqaure that should be picked with RGB values of my choice.  It also implements logic to see if the color picked matches the color clicked.

---
+++ colorGame.js
@@ -0,0 +1,36 @@
+var colors=[
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
+
+var squares=document.querySelectorAll(".square")
+var pickedColor=colors[3]
+var colorDisplay=document.querySelector("#colorDisplay")
+
+colorDisplay.textContent=pickedColor
+
+for(var i=0; i<squares.length; i++)
+{
+    //Add initial colors to squares
+    squares[i].style.backgroundColor=colors[i]
+
+    //Add click listeners to squares
+    squares[i].addEventListener("click",function()
+    {
+        //Grab color of picked square
+        var clickedColor=this.style.backgroundColor
+        //Compare color to pickedColor
+        if(clickedColor===pickedColor)
+        {
+            alert("Correct")
+        }
+        else
+        {
+            alert("Wrong")
+        }
+    })
+}
